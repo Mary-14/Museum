776eae8639db4a1db4d5a825a17a5582e040607d
Banana Android Z <ndrdbnnz@gmail.com>
2018-05-21 19:33:48 +0300

    not working(((

--- app/src/main/java/com/sobol/user/test_project/Museum.java
+++ app/src/main/java/com/sobol/user/test_project/Museum.java
@@ -9,9 +9,10 @@ public class Museum implements Serializable {
     public double lgn;
     public String place_id;
 
-    public Museum(double lat, double lgn, String place_id) {
+    public Museum(double lat, double lng, String placeId, String place_id) {
         this.lat = lat;
         this.lgn = lgn;
         this.place_id = place_id;
     }
+
 }
